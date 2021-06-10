e0adf154e214144db1b59b83ae20d80a84e70341
ElSaber10 <83537548+ElSaber10@users.noreply.github.com>
2021-06-10 15:28:46 -0600

    ff

---
+++ Ejercicio2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Random;
+public class Ejercicio2 {
+	public static void main(String[] args){
+	System.out.println("Bienvenido ingrese si numero");
+	Scanner sc = new Scanner(System.in);
+	int N = sc.nextInt();
+	for(int i = N; 1 <= i; i--){
+	System.out.println(i);
+	}
+}
+}
